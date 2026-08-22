2a8d3534a7a4ddc36e7c4cd576b1e3f08d0afb63
Muhammad Sefat <muhammadsefat55@gmail.com>
2026-08-23 00:15:39 +0600

    start woring on the user setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,16 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './common/prisma/prisma.module.js';
 import { AuthModule } from './modules/auth/auth.module.js';
+import { UsersModule } from './modules/users/users.module.js';
+import { ImagesModule } from './modules/images/images.module.js';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    AuthModule,
+    UsersModule,
+    ImagesModule,
+  ],
 })
 export class AppModule {}
