a9014f2fbec5f804020e23e9dabe19e83feacb4b
ArunRoy404 <arunroy0065@gmail.com>
2026-09-07 00:08:00 +0600

    feat: implement NotificationsService for SMS, email, and FCM and add Postman collection

--- src/modules/notifications/notifications.service.ts
+++ src/modules/notifications/notifications.service.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-assignment */
 import { Injectable, Logger } from '@nestjs/common';
 import { InjectQueue } from '@nestjs/bullmq';
 import { Queue } from 'bullmq';
